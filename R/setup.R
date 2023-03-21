6bc3f7a78c7c338914980fbddeb53077d5b77bdd
Erickcufe <erickcufe94@gmail.com>
2023-03-21 15:39:00 -0600

    .

---
+++ R/setup.R
@@ -0,0 +1,11 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c("SCENIC", "GENIE3", "GRNBoost2", "AUCell", "JASPAR2022", "Seurat", "igraph"))
+
+library(SCENIC)
+library(GENIE3)
+library(GRNBoost2)
+library(AUCell)
+library(JASPAR2022)
+library(Seurat)
+library(igraph)
